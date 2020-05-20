d60ac32e5b359dd5457e864c3ff1279d4bcc4154
Maren Beam <marenbeam@mailbox.org>
2020-05-20 06:41:15 -0400

    improve installer

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -euf pipefail
 binDir=$HOME/.local/bin
 stuffDir=$HOME/.local/share/thoughts
 
-if [ -d $stuffDir ]; then
+if [ -d "$stuffDir" ]; then
     printf "Thoughts is already installed. Delete everything and reinstall? [y/n]: "
     read -r reply
     if [ ! "$reply" = "y" ]; then
@@ -15,23 +15,18 @@ if [ -d $stuffDir ]; then
     torch=true
 fi
 
-mkdir -p $stuffDir
-cp .head.html $stuffDir
-cp .foot.html $stuffDir
+mkdir -p "$stuffDir"
+cp .head.html "$stuffDir"
+cp .foot.html "$stuffDir"
 touch $stuffDir/.rawthoughts.html
 echo '*' > $stuffDir/.gitignore
-echo '!thoughts.html' >> $stuffDir/.gitignore
-echo '!.gitignore' >> $stuffDir/.gitignore
-echo '!.rawthoughts.html' >> $stuffDir/.gitignore
+echo '!thoughts.html' >> "$stuffDir"/.gitignore
+echo '!.gitignore' >> "$stuffDir"/.gitignore
+echo '!.rawthoughts.html' >> "$stuffDir"/.gitignore
 
-
-if [ -f $binDir ]; then
-    echo "$binDir already exists" 1>&2
-    exit 1
-fi
-
-cp thoughts $binDir
-chmod +x $binDir/thoughts
+mkdir -p "$binDir"
+cp thoughts "$binDir"
+chmod +x "$binDir"/thoughts
 
 echo 'Done! Add $HOME/.local/bin to your PATH.'
 exit 0
